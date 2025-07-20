0fcfe9a49bd2684efbd9bb97dfd4cfc07d6e5981
jumin20099 <jumin20099@gmail.com>
2025-07-20 12:03:25 +0900

    cors 핸들러 변경

--- snapfit-backend/src/main/java/com/snapfit/api/config/WebConfig.java
+++ snapfit-backend/src/main/java/com/snapfit/api/config/WebConfig.java
@@ -1,19 +1,8 @@
 package com.snapfit.api.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+public class WebConfig {
+    // CORS 설정은 SecurityConfig에서 관리
 }
