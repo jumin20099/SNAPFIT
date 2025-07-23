afbd8cf8b39e8902030f49bfbcdd3c73eda70db4
jumin20099 <jumin20099@gmail.com>
2025-07-24 00:08:23 +0900

    AvatarCanvas 컴포넌트 및 three.js 의존성 추가

---
+++ src/components/avatar-canvas.tsx
@@ -0,0 +1,22 @@
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stage } from '@react-three/drei';
+import { Suspense } from 'react';
+
+export default function AvatarCanvas() {
+  return (
+    <Canvas style={{ width: '100%', height: '500px' }} camera={{ position: [0, 1.6, 3] }}>
+      <ambientLight intensity={0.7} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <Suspense fallback={null}>
+        {/* TODO: 실제 아바타 GLB 모델 로드 */}
+        <Stage>
+          <mesh>
+            <boxGeometry args={[1, 2, 0.5]} />
+            <meshStandardMaterial color="lightgray" />
+          </mesh>
+        </Stage>
+      </Suspense>
+      <OrbitControls enablePan={false} />
+    </Canvas>
+  );
+} 
